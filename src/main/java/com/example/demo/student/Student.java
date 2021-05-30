50716538e1681e48fee42a8c5c9a627f3fd552ea
Guysnacho <guysnacho2@gmail.com>
2021-05-30 09:52:31 -0500

    first commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+    private LocalDate dob;
+    private String email;
+
+    public Student(long id, String name, int age, LocalDate dob, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+}
